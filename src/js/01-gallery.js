48f7db010f5e4664a37a672fe876bded590cbb0f
SofiiaKoshelieva <sofikosh58@gmail.com>
2022-01-22 16:47:25 +0200

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,56 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+const cardsMarkup = createImgCardsMarkup(galleryItems);
+
+galleryEl.addEventListener("click", onGalleryClick);
+galleryEl.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createImgCardsMarkup(galleryItems){
+    
+    return galleryItems.map(({preview, original, description}) => {
+        return   `
+        <div class="gallery__item">
+        <a class="gallery__link" href="${original}">
+          <img
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+        </div>
+        `;
+    }).join('');
+};
+
+//делегирование
+function onGalleryClick(event){
+    event.preventDefault();
+    const isImgEl = event.target.classList.contains("gallery__image");
+
+    if(!isImgEl){
+        return;
+    }
+
+    openModalLightbox(event);
+    window.addEventListener('keydown', onEscKeyPress);
+};
+
+let instance;
+
+function openModalLightbox(event) {
+    instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+  `);
+    instance.show();
+};
+
+
+function onEscKeyPress(event) {
+    if (event.code === 'Escape' ||'return') {
+        instance.close();
+        window.removeEventListener('keydown', onEscKeyPress);
+    };
+};
